0a94ca262fdced68785e9348f8e1b59ac484b785
gulshanmammadova <gulshanfm@code.edu.az>
2024-05-08 10:36:44 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  //content: ["./public/**/*.{html,js}"],-bele de ola biler
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
